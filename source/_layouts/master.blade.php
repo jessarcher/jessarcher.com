23664dcb1a9cca3502159fe84e5c367b012949d9
Jess Archer <jess@jessarcher.com>
2018-10-25 21:48:40 +1000

    Copy existing homepage code

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Jess Archer</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,9 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+
+<div class="h-100 d-flex justify-content-center align-items-center">
+    <img src="assets/images/jessarcher.png" alt="Jess Archer" height="230" width="230">
+</div>
+
 @endsection
