7d843e6825d0482ae149527c3773f744168d3fa7
Jess Archer <jess@jessarcher.com>
2018-10-26 23:11:03 +1000

    Add a 404 template

---
+++ source/404.blade.php
@@ -0,0 +1,35 @@
+---
+permalink: 404.html
+---
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="flex items-center justify-center h-screen">
+    <div class="text-center">
+        <a href="/">
+            <img
+                src="/assets/images/jessarcher.png"
+                alt="Jess Archer"
+                height="125"
+                width="125"
+                class="border-white border-4 rounded-full shadow-md mb-4"
+            >
+        </a>
+
+        <div class="text-grey-darkest text-xl font-semibold mb-4">You look a little lost...</div>
+
+        <a
+            class="
+                no-underline
+                align-baseline
+                text-grey-dark
+                hover:text-grey-darker
+            " href="/"
+        >
+            <i class="text-grey fas fa-long-arrow-alt-left"></i> Return Home
+        </a>
+    </div>
+</div>
+
+@endsection
