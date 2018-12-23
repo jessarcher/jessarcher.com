3523d819689829f389a08524ae58bb1b0d4b9ff7
Jess Archer <jess@jessarcher.com>
2018-12-23 15:12:36 +1000

    Switch to laravel-mix-purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 var tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -18,38 +19,6 @@ mix.webpackConfig({
     ]
 });
 
-if (mix.inProduction()) {
-    let glob = require("glob-all");
-    let PurgecssPlugin = require("purgecss-webpack-plugin");
-
-    class TailwindExtractor {
-        static extract(content) {
-            return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-        }
-    }
-
-    mix.webpackConfig({
-        plugins: [
-            new PurgecssPlugin({
-
-                // Specify the locations of any files you want to scan for class names.
-                paths: glob.sync([
-                    path.join(__dirname, "source/**/*.php"),
-                ]),
-                extractors: [
-                    {
-                        extractor: TailwindExtractor,
-
-                        // Specify the file extensions to include when scanning for
-                        // class names.
-                        extensions: ["html", "js", "php", "vue"]
-                    }
-                ]
-            })
-        ]
-    });
-}
-
 mix.js('source/_assets/js/main.js', 'js')
     .sourceMaps()
     .sass('source/_assets/sass/main.scss', 'css')
@@ -57,6 +26,11 @@ mix.js('source/_assets/js/main.js', 'js')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
+    })
+    .purgeCss({
+        extensions: ['html', 'md', 'js', 'php', 'vue'],
+        folders: ['source'],
+        whitelistPatterns: [/language/, /hljs/],
     })
     .version();
 
