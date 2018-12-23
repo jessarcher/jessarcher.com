e5b9aafcc268c9500e8f96efd08c6aa5aabf054c
Jess Archer <jess@jessarcher.com>
2018-12-23 16:06:45 +1000

    Update configuration

--- config.php
+++ config.php
@@ -1,10 +1,42 @@
 <?php
 
 return [
-    'name' => 'Jess Archer',
+    'siteName' => 'Jess Archer',
+    'siteDescription' => 'Web App Developer',
     'baseUrl' => '',
     'production' => false,
+
+    // Collections
     'collections' => [
-        'posts',
+        'posts' => [
+            'sort' => '-date',
+            'path' => 'blog/{filename}',
+        ],
     ],
+
+    // Helpers
+    'getDate' => function ($page) {
+        return DateTime::createFromFormat('U', $page->date);
+    },
+
+    'excerpt' => function ($page, $length = 255) {
+        $cleaned = strip_tags(
+            preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $page->getContent()),
+            '<code>'
+        );
+
+        $truncated = substr($cleaned, 0, $length);
+
+        if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
+            $truncated .= '</code>';
+        }
+
+        return strlen($cleaned) > $length
+            ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
+            : $cleaned;
+    },
+
+    'isActive' => function ($page, $path) {
+        return ends_with(trimPath($page->getPath()), trimPath($path));
+    },
 ];
---
+++ config.production.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'baseUrl'    => 'https://jessarcher.com',
+    'production' => true,
+];
