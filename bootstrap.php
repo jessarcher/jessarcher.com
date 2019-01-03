1b8f19da3d5847ebe54626b0be626d2bd5088478
Jess Archer <jess@jessarcher.com>
2019-01-03 23:24:45 +1000

    Generate index and sitemap

--- bootstrap.php
+++ bootstrap.php
@@ -15,3 +15,6 @@ use TightenCo\Jigsaw\Jigsaw;
  *     // Your code here
  * });
  */
+
+$events->afterBuild(App\Listeners\GenerateSitemap::class);
+$events->afterBuild(App\Listeners\GenerateIndex::class);
