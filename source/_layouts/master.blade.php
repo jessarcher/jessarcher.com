70309eeb449b6d7a6738090721c29fec9f785c07
Jess Archer <jess@jessarcher.com>
2019-11-04 17:22:31 +1000

    Add opengraph image

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -9,6 +9,7 @@
         <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
         <meta property="og:type" content="website" />
         <meta property="og:url" content="{{ $page->getUrl() }}"/>
+        <meta property="og:image" content="{{ $page->baseUrl }}/assets/images/logo.png"/>
         <meta property="og:description" content="{{ $page->siteDescription }}" />
 
         <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
