be9ccdc9dac51cd98d4e61051b692a5cf1bc7a85
Jess Archer <jess@jessarcher.com>
2019-11-12 17:15:43 +1000

    Fix build issues

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,15 +4,8 @@ const build = require('./tasks/build.js');
 require('laravel-mix-tailwind');
 require('laravel-mix-purgecss');
 
-mix.options({
-    postCss: [
-        require('postcss-import'),
-        require('postcss-nested'),
-    ]
-});
-
 mix.disableSuccessNotifications();
-mix.setPublicPath('source');
+mix.setPublicPath('source/assets/build/');
 mix.webpackConfig({
     plugins: [
         build.jigsaw,
@@ -26,15 +19,23 @@ mix.webpackConfig({
         ]),
     ]
 });
+mix.options({
+    processCssUrls: false,
+    postCss: [
+        require('postcss-import'),
+        require('postcss-nested'),
+    ]
+});
 
-mix.js('source/_assets/js/main.js', 'assets/build/js')
-   .postCss('source/_assets/css/main.css', 'assets/build/css')
-   .tailwind('tailwind.config.js');
-
-if (mix.inProduction()) {
-  mix
-    .version()
-    .purgeCss({
-      folders: ['source'],
-    });
-}
+mix.js('source/_assets/js/main.js', 'js')
+   .sourceMaps()
+   .postCss('source/_assets/css/main.css', 'css/main.css')
+   .sourceMaps()
+   .tailwind('tailwind.config.js')
+   .purgeCss({
+        extensions: ['html', 'md', 'js', 'php', 'vue'],
+        folders: ['source'],
+        whitelistPatterns: [/language/, /hljs/],
+   })
+  .copy('./node_modules/font-proxima-nova-scss/fonts/', 'source/assets/build/fonts/vendor/proxima-nova/')
+  .version();
