9f3cdc0a221a9ee3ee65dc5f795f61ceb9d003f2
Jess Archer <jess@jessarcher.com>
2020-02-09 23:08:58 +1000

    Update speaking page

--- source/speaking.blade.php
+++ source/speaking.blade.php
@@ -10,16 +10,16 @@ title: Speaking
 
 @section('main')
 
-    <h1 class="uppercase tracking-wide font-bold text-gray-600">Speaking</h1>
+    <h1 class="font-bold tracking-wide text-gray-600 uppercase">Speaking</h1>
 
     <div class="mt-6 markdown">
         <p>In 2019 I started my speaking career at <a href="https://laracon.com.au" target="_blank">Laracon AU</a> where I gave a talk titled <em>&ldquo;The Laravel Developer's Guide to Vue SPAs&rdquo;</em>.</p>
 
         <img src="/assets/images/jess-archer-speaks.jpg" alt="Jess Archer speaking at Laracon AU 2019" />
 
-        <p>The full talk will be posted on the <a href="https://www.youtube.com/channel/UCYGmktKAAJWnqGGaH9DcTqw" target="_blank">Laracon AU YouTube channel</a> soon, so be sure to subscribe to the channel and <a href="https://twitter.com/jessarchercodes" target="_blank">follow me on Twitter</a> to be notified when it's available!</p>
+        <p><a href="https://www.youtube.com/watch?v=Zv4bUXEwl20" target="_blank">Watch the Laravel Developer's Guide to Vue SPAs on YouTube</a></p>
 
-        <p>In the mean time, you can <a href="/blog/closing-modals-with-the-back-button-in-a-vue-spa/">read a blog post</a> about one of the most popular things I talked about.</p>
+        <p><a href="/blog/closing-modals-with-the-back-button-in-a-vue-spa/">Read a blog post about one of the most popular things I talked about</a></p>
     </div>
 
 @endsection
