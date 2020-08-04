25ff42cf7e5f9722ac8ed235279c1851b0ac927a
Jess Archer <jess@jessarcher.com>
2020-08-04 21:08:08 +1000

    Update dependencies

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'siteName' => 'Jess Archer',
     'siteDescription' => 'Full-stack web app developer from Brisbane, Australia',
@@ -41,6 +43,6 @@ return [
     },
 
     'isActive' => function ($page, $path) {
-        return ends_with(trimPath($page->getPath()), trimPath($path));
+        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
 ];
--- listeners/GenerateSitemap.php
+++ listeners/GenerateSitemap.php
@@ -2,8 +2,9 @@
 
 namespace App\Listeners;
 
-use samdark\sitemap\Sitemap;
 use TightenCo\Jigsaw\Jigsaw;
+use Illuminate\Support\Str;
+use samdark\sitemap\Sitemap;
 
 class GenerateSitemap
 {
@@ -37,6 +38,6 @@ class GenerateSitemap
 
     public function isExcluded($path)
     {
-        return str_is($this->exclude, $path);
+        return Str::is($this->exclude, $path);
     }
 }
