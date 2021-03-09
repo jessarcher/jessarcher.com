446e859d4a38b04343c0a5c041e779b569e93fa5
Laravel Shift <shift@laravelshift.com>
2021-03-09 12:07:17 +0000

    Convert to Tailwind 2.0 colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-module.exports = {"theme":{"extend":{"fontFamily":{"sans":"Proxima Nova, -apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, \"Helvetica Neue\", Arial, \"Noto Sans\", sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\", \"Noto Color Emoji\""},"screens":{"print":{"raw":"print"}}}},"variants":{"width":["responsive","focus"],"borderColor":["responsive","hover","focus","group-hover"]},"plugins":[require('@tailwindcss/custom-forms'),function({ addUtilities }) {
+const colors = require('tailwindcss/colors')
+
+module.exports = {"theme":{"extend":{"fontFamily":{"sans":"Proxima Nova, -apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, \"Helvetica Neue\", Arial, \"Noto Sans\", sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\", \"Noto Color Emoji\""},"screens":{"print":{"raw":"print"}},"colors":{"gray":colors.blueGray,"orange":colors.orange,"yellow":colors.yellow,"green":colors.green,"teal":colors.teal,"blue":colors.lightBlue,"purple":colors.purple}}},"variants":{"width":["responsive","focus"],"borderColor":["responsive","hover","focus","group-hover"]},"plugins":[require('@tailwindcss/custom-forms'),function({ addUtilities }) {
       const newUtilities = {
         '.transition-fast': {
           transition: 'all .2s ease-out',
